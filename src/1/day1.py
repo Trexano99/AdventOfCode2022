6f35d3aac54559cc2a11c8cb7bae2a1b4933ef9e
Massimiliano Visconti <viscontimax@hotmail.it>
2022-12-01 16:01:46 +0100

    First day

--- src/1/day1.py
+++ src/1/day1.py
@@ -3,6 +3,6 @@ import functools
 linee = open("input.txt").readlines()
 
 allElvesCal = functools.reduce(lambda tot, line : (line == "\n" and tot+[0]) or (tot[:-1]+[tot[-1]+int(line.replace("\n",""))]), linee, [0])
+
 print(max(allElvesCal))
 print(sum(sorted(allElvesCal)[-3:]))
-
